3e3bd1e84d0ffd9dfb442da45b159bec70c2808d
Lukas Kraic <lukas.kraic@alanata.sk>
2026-02-10 21:27:36 +0100

    chore: bump version to 1.4.4

--- setup.php
+++ setup.php
@@ -29,7 +29,7 @@ use GlpiPlugin\EntraHierarchy\EntraSync;
 use GlpiPlugin\EntraHierarchy\EntraAuth;
 use Glpi\Http\Firewall;
 
-define('PLUGIN_ENTRAHIERARCHY_VERSION', '1.4.3');
+define('PLUGIN_ENTRAHIERARCHY_VERSION', '1.4.4');
 
 /**
  * Initialize the plugin
